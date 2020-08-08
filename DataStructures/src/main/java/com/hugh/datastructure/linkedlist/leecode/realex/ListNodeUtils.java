aba0b97c24f3131a08c79857e6ae5ac1f3d685ce
FlyMeToTheMars <flyhobo@live.com>
2020-08-08 20:38:06 +0800

    ListNodeUtils V1.0.0

--- DataStructures/src/main/java/com/hugh/datastructure/linkedlist/leecode/realex/ListNodeUtils.java
+++ DataStructures/src/main/java/com/hugh/datastructure/linkedlist/leecode/realex/ListNodeUtils.java
@@ -1,5 +1,7 @@
 package com.hugh.datastructure.linkedlist.leecode.realex;
 
+import java.util.ArrayList;
+
 /**
  * @Author Fly.Hugh
  * @Date 2020/8/7 18:26
@@ -7,14 +9,34 @@ package com.hugh.datastructure.linkedlist.leecode.realex;
  **/
 public class ListNodeUtils {
     public static void main(String[] args) {
-
+        ListNode head = generateLinkedList(1, 2, 3, 4,5,6,7);
+        traverseLinkListFromFirst(head);
     }
 
     public static ListNode generateLinkedList(int... nums){
 
+        ListNode first = new ListNode(nums[0]);
+        ListNode second;
+        ListNode head = first;
 
+        if(nums.length == 1) {
+            return first;
+        }
 
+        int i;
+        for (i = 0; i < nums.length - 1; i++) {
+            second = new ListNode(nums[i+1]);
+            first.next = second;
+            first = second;
+        }
+
+        return head;
     }
 
-    public static void
+    public static void traverseLinkListFromFirst(ListNode head){
+        while(head != null) {
+            System.out.print(head.val + ",");
+            head = head.next;
+        }
+    }
 }
