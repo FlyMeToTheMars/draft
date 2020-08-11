dc1b0d2cefbab6529f834c43b44dd9eec1f42bfb
FlyMeToTheMars <flyhobo@live.com>
2020-08-12 05:34:00 +0800

    binary tree V0.0.1

---
+++ DataStructures/src/main/java/com/hugh/datastructure/binarytree/utils/TreeNode.java
@@ -0,0 +1,17 @@
+package com.hugh.datastructure.binarytree.utils;
+
+/**
+ * @program: draft
+ * @description: 二叉树节点
+ * @author: Fly.Hugh
+ * @create: 2020-08-12 04:58
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
