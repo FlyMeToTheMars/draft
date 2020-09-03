9ec373a1ef327854ad0b837f6126d3a9757cace3
gaoming.liu <liugaoming@jimilab.com>
2020-09-03 11:09:52 +0800

    Trie iterate V0.0.1

---
+++ DataStructures/src/test/java/trie/Trie.java
@@ -0,0 +1,59 @@
+package trie;
+
+/**
+ * @Author Fly.Hugh
+ * @Date 2020/9/3 11:08
+ * @Version 1.0
+ **/
+public class Trie {
+
+    TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i ++) {
+            char c = word.charAt(i);
+            if(p.child[c - 'a'] == null) {
+                p.child[c - 'a'] = new TrieNode();
+            }
+            p = p.child[c - 'a'];
+        }
+        p.isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i ++) {
+            char c = word.charAt(i);
+            if(p.child[c - 'a'] == null) {
+                return false;
+            }
+            p = p.child[c - 'a'];
+        }
+        /*if(p.isEnd == true) {
+            return true;
+        }
+        return false;*/
+        return p.isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode p = root;
+        for(int i = 0; i < prefix.length(); i ++) {
+            char c = prefix.charAt(i);
+            if(p.child[c - 'a'] == null) {
+                return false;
+            }
+            p = p.child[c - 'a'];
+        }
+        return true;
+    }
+}
---
+++ DataStructures/src/test/java/trie/TrieNode.java
@@ -0,0 +1,15 @@
+package trie;
+
+/**
+ * @Author Fly.Hugh
+ * @Date 2020/9/3 11:08
+ * @Version 1.0
+ **/
+public class TrieNode {
+    TrieNode[] child;
+    boolean isEnd;
+    public TrieNode() {
+        this.child = new TrieNode[26];
+        this.isEnd = false;
+    }
+}
