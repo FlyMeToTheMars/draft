3fee0f9cbde9ff6829161f11f7a2fb7a8f05cbf6
gaoming.liu <liugaoming@jimilab.com>
2020-09-03 11:32:01 +0800

    subStringTest V1.0.0

---
+++ src/main/java/com/hugh/draft/str/SubStringTest.java
@@ -0,0 +1,14 @@
+package com.hugh.draft.str;
+
+/**
+ * @Author Fly.Hugh
+ * @Date 2020/9/3 11:26
+ * @Version 1.0
+ **/
+public class SubStringTest {
+    public static void main(String[] args) {
+        String str = "helloworld";
+        String s = str.substring(1);
+        System.out.println(s);
+    }
+}
