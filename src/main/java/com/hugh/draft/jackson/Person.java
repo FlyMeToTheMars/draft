c4df9354d5cc2eeda044d806746d798eb9f1cf7f
FlyMeToTheMars <flyhobo@live.com>
2020-09-28 04:23:23 +0800

    Regex V1.2.1

--- src/main/java/com/hugh/draft/jackson/Person.java
+++ src/main/java/com/hugh/draft/jackson/Person.java
@@ -15,7 +15,8 @@ public class Person {
 
     @JsonFormat(
             shape = JsonFormat.Shape.STRING,
-            pattern = "yyyy-MM-dd HH:mm:ss"
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone="GMT+8"
     )
     private Date time;
 
